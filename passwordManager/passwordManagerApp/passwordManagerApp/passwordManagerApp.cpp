2ba516423b8b8f582b7316b01aec24aa6ffbbc9f
ireed <ireed@users.noreply.github.com>
2016-05-24 14:41:49 -0500

    Add files via upload
    Added functions and re-designed with the purpose of using as a native middle-layer between enclave and GUI.

--- passwordManager/passwordManagerApp/passwordManagerApp/passwordManagerApp.cpp
+++ passwordManager/passwordManagerApp/passwordManagerApp/passwordManagerApp.cpp
@@ -15,14 +15,78 @@
 
 using namespace std;
 
-//int _tmain(int argc, _TCHAR* argv[])
+sgx_enclave_id_t enclaveId = NULL;
+sgx_launch_token_t token = {0};
+int updated;
+
+
+bool authenticate(char *username, char *password)
+{
+	int ret = -1;
+	authenticate(enclaveId,&ret,username,password,MAX_BUF_LEN);
+	if(ret < 0) return false;
+	return true;
+}
+
+bool decrypt(string *password)
+{
+	//do crypto stuff
+	return true;
+}
+
+bool destroyEnclave()
+{
+	if(sgx_destroy_enclave(enclaveId) != SGX_SUCCESS)
+		//printf("Error %x: cant destroy enclave\n", ret);
+		return false;
+	return true;
+}
+
+bool destroyEnclave(sgx_enclave_id_t id)
+{
+	if(sgx_destroy_enclave(id) != SGX_SUCCESS)
+		return false;
+	return true;
+}
+
+
+bool encrypt(string *password)
+{
+	//do crypto stuff
+	return true;
+}
+
+
+
+int getSize()
+{
+	int size;
+	return getSize(enclaveId);
+	return size;
+}
+
+bool initializeEnclave()
+{
+	if(sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, 
+		&enclaveId, NULL) != SGX_SUCCESS)
+		//printf("Error %#x: cannot create enclave\n", ret);
+		return false;
+	return true;
+}
+
+bool initializeEnclave(sgx_enclave_id_t *id)
+{
+	if(sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, 
+		id, NULL) != SGX_SUCCESS)
+		return false;
+	return true;
+}
+
+//main function used to debug/demo enclave without GUI
 int main()
 {
-	sgx_enclave_id_t enclaveId = NULL;
 	sgx_status_t ret = SGX_SUCCESS;
-	sgx_launch_token_t token = {0};
-	sgx_launch_token_t *launchToken = NULL;
-	int updated, retval, option=1;
+	int retval, option=1;
 	char *username, *password, *input;
 
 	/*if(sgx_is_capable(&updated) != SGX_ENABLED) 
@@ -36,9 +100,7 @@ int main()
 	password = (char*)malloc(MAX_BUF_LEN*sizeof(char));
 	input = (char*)malloc(MAX_BUF_LEN*sizeof(char));
 
-	ret = sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, 
-		&enclaveId, NULL);
-	if(ret != SGX_SUCCESS)
+	if(!initializeEnclave())
 	{
 		printf("Error %#x: cannot create enclave\n", ret);
 		return -1;
@@ -155,7 +217,7 @@ int main()
 
 
 
-	if(sgx_destroy_enclave(enclaveId) != SGX_SUCCESS)
+	if(!destroyEnclave())
 	{
 		printf("Error %x: cant destroy enclave\n", ret);
 		return -1;
